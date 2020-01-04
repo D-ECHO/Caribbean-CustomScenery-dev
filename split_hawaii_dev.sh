b17f813b33ccc9acf0747b6e979a12653618f9ed
Delta Echo <echoair@t-online.de>
2020-01-04 13:06:47 +0100

    Remove unrelated files

--- split_hawaii_dev.sh
+++
@@ -1,3 +0,0 @@
-mkdir haw_town haw_urban
-ogr2ogr -f "ESRI Shapefile" -select 'LUDCODE' -where "LUDCODE = 'R'" haw_town/slud_town.shp slud.shp/slud.shp
-ogr2ogr -f "ESRI Shapefile" -select 'LUDCODE' -where "LUDCODE = 'U'" haw_urban/slud_urban.shp slud.shp/slud.shp
